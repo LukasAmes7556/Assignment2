ae6d7d68c6c47b6430749013e67978e31a778c7e
Lukas Ames <lames1@my.centennialcollege.ca>
2022-10-21 15:09:23 -0400

    Initial Commit

---
+++ config/config.js
@@ -0,0 +1,3 @@
+export const Secret = "MySecret";
+// export const MongoURI  = "mongodb://127.0.0.1/media"
+export const MongoURI  = "mongodb+srv://Lukas337:z4i7vLEKiLULERg@cluster0.9d1pdla.mongodb.net/portfolio?retryWrites=true&w=majority" 
